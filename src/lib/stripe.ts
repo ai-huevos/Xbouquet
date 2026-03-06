d3f06fc7a0e456807cfc98fe616b1fb8cecb0420
Daniel Cardona <hola@huevos.ai>
2026-03-06 09:21:21 -0500

    test(e2e): stub checkout.spec.ts pending db seed

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     // https://github.com/stripe/stripe-node#configuration
-    apiVersion: '2025-02-24.acacia',
+    apiVersion: '2026-02-25.clover',
     appInfo: {
         name: 'Xpress Buke',
         url: 'http://localhost:3000',
